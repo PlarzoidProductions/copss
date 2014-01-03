111e87b1b04768ed452b3e10816ea7471795d992
Plarzoid <plarzoid@gmail.com>
2014-01-02 23:04:44 -0500

    Added default achievement data to build script Tweaked Achievement listing display

--- database/scripts/build.sql
+++ database/scripts/build.sql
@@ -13,3 +13,6 @@ SOURCE ../creation/add_locations.sql
 SELECT 'Adding default PP Data...' as '';
 SOURCE ../creation/add_wm_hordes.sql
 SOURCE ../creation/add_misc_pp.sql
+
+SELECT 'Adding default Achievements...' as '';
+SOURCE ../creation/add_achievements.sql
